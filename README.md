508044b11240a9dd9aa84c40d55cbd50adc1705c
@awhitehatter <awhitehttr@gmail.com>
2015-01-14 22:29:54 -0700

    initial release

---
+++ README.md
@@ -0,0 +1,28 @@
+# About
+Mailoney is a SMTP Honeypot I wrote just to have fun learning Python. The Open Relay module emulates an Open Relay and writes attempted emails to a log file. Similarly, the Authentication modules will capture credentials and write those to a log file. 
+
+# Usage
+
+You'll likely need to run this with elevated permissions as required to open sockets on special ports.
+
+```
+python mailoney.py -s mailbox <options>
+
+  -h, --help            Show this help message and exit
+  -i <ip address>       The IP address to listen on (defaults to localhost)
+  -p <port>             The port to listen on (defaults to 25)
+  -s mailserver         This will generate a fake hostname
+  -t <type>		HoneyPot type
+	open_relay	Emulates an open relay 
+	postfix_creds   Emulates PostFix authentication server, collects credentials
+                        
+examples:
+python mailoney.py -s mailbox -i 10.10.10.1 -p 990 -t postfix_creds
+
+```
+
+# ToDo 
+ [ ] Add modules for EXIM, Microsoft, others
+ [ ] Build in Error Handling
+ [ ] Add a Daemon flag to background process.
+
---
+++ mailoney.py
@@ -0,0 +1,39 @@
+__author__ = '@awhitehatter'
+__version__ = '0.1'
+
+'''
+add some nice comments here
+'''
+
+import argparse
+import sys
+import modules.postfix_creds
+import modules.open_relay
+
+#parse the command line arguments to set the variables for the server
+parser = argparse.ArgumentParser(description="Command line arguments")
+parser.add_argument('-i',action='store', metavar='<ip address>', default='0.0.0.0', help='The IP address to listen on')
+parser.add_argument('-p',action='store', metavar='<port>', default='25', help='The port to listen on')
+parser.add_argument('-s',action='store', metavar='mailserver', required=True, help='A Name that\'ll show up as the mail server name')
+parser.add_argument('-t',action='store', choices=['open_relay', 'postfix_creds'], required=True, help='honeypot type')
+args = parser.parse_args()
+
+#set the IP address variables
+bind_ip = args.i
+bind_port = int(args.p)
+srvname = args.s
+
+banner = ('''
+****************************************************************
+\tMailoney - A Simple SMTP Honeypot - Version: {}
+****************************************************************
+'''.format(__version__))
+
+#call server type module, based on parsed arguments
+if args.t == 'postfix_creds':
+    modules.postfix_creds.pfserver()
+
+elif args.t == 'open_relay':
+    modules.open_relay.or_module()
+else:
+    print 'I don\'t know what this module is...Exiting...\r\n'
