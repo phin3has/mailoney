a75fc9161f516f1f9cdef255015fd973063839e1
Reyes <r.eyesweb999@gmail.com>
2015-04-19 00:54:52 -0400

    Create start.sh
    Run the honeypot under restricted privileges
    usage of nohup should be removed as soon as mailoney.py will have background process capability.

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Run the honeypot under restricted privileges
+
+if [[ $USER != "root" ]]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+display_usage() {
+    echo -e "\nUsage: start.sh -s <fake mailserver name> -t <honeypot type> [Optional arguments]\n"
+    echo "-s <fake mailserver name>	The name that will show up as the mail server name. (Example: -s mailserver)"
+    echo "-t <honeypot type>		HoneyPot type. Choices:"
+	echo "    postfix_creds 	Emulates PostFix authentication server, collects credentials"
+	echo "    open_relay    	Emulates an open relay"
+    echo "-i <ip address>		Optional argument - The IP address to listen on. (Default value is 0.0.0.0)"
+    echo "-p <port>		Optional argument - The port to listen on. (Default value is 25)"
+    echo -e "-h, --help		Show this help message and exit\n"
+}
+
+# check whether user had supplied -h or --help . If yes display usage
+if [[ ( $# == "--help") ||  $# == "-h" ]] ; then
+    display_usage
+    exit 0
+fi
+
+# if less than 4 arguments supplied, display usage
+if [  $# -le 3 ] ; then
+    display_usage
+    exit 1
+fi
+
+# put arguments into a variable
+script_arguments=$@
+
+echo "Starting mailoney in the background...\n"
+cd $(dirname $0)
+su mailoney -c "nohup authbind --deep python mailoney.py $script_arguments > $(dirname $0)/logs/mailoney.out &"
