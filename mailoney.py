36b255f8f5d6a96e3fbee99a9cf191600f0fdd13
bwall <bwall9809@gmail.com>
2015-10-14 21:37:55 -0700

    Adding initial version of schizo_open_relay

--- mailoney.py
+++ mailoney.py
@@ -6,20 +6,22 @@ add some nice comments here
 '''
 
 import argparse
-import sys
 import os
+
 import modules.postfix_creds
 import modules.open_relay
+import modules.schizo_open_relay
 
-#parse the command line arguments to set the variables for the server
+
+# parse the command line arguments to set the variables for the server
 parser = argparse.ArgumentParser(description="Command line arguments")
 parser.add_argument('-i',action='store', metavar='<ip address>', default='0.0.0.0', help='The IP address to listen on')
 parser.add_argument('-p',action='store', metavar='<port>', default='25', help='The port to listen on')
 parser.add_argument('-s',action='store', metavar='mailserver', required=True, help='A Name that\'ll show up as the mail server name')
-parser.add_argument('-t',action='store', choices=['open_relay', 'postfix_creds'], required=True, help='honeypot type')
+parser.add_argument('-t',action='store', choices=['open_relay', 'postfix_creds', 'schizo_open_relay'], required=True, help='honeypot type')
 args = parser.parse_args()
 
-#set the IP address variables
+# set the IP address variables
 bind_ip = args.i
 bind_port = int(args.p)
 srvname = args.s
@@ -30,15 +32,17 @@ banner = ('''
 ****************************************************************
 '''.format(__version__))
 
-#create log directory (thanks @Bifrozt_Dev)
+# create log directory (thanks @Bifrozt_Dev)
 if not os.path.isdir('logs'):
         os.mkdir('logs')
 
-#call server type module, based on parsed arguments
+# call server type module, based on parsed arguments
 if args.t == 'postfix_creds':
     modules.postfix_creds.pfserver()
-
 elif args.t == 'open_relay':
     modules.open_relay.or_module()
+elif args.t == 'schizo_open_relay':
+    modules.schizo_open_relay.module()
 else:
     print 'I don\'t know what this module is...Exiting...\r\n'
+
